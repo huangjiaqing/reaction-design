ee34fe49b36c7c5cc6318d3aee579d214507de8f
Huang Jiaqing <657593825@qq.com>
2018-08-20 18:10:23 +0800

    fix: header

---
+++ src/routes/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Content.css';
+
+export default class Content extends React.Component {
+
+  render() {
+
+    return (
+      <div className={styles.main}>
+        <div>
+          zhenhao
+        </div>
+      </div>
+    );
+  }
+}
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Icon, Popover, Tree, Input, Avatar, Badge, Card } from 'antd';
+import { Layout, Icon, Popover, Tree, Input, Avatar, Badge, Card, Table } from 'antd';
 import styles from './IndexPage.css';
 
 const TreeNode = Tree.TreeNode;
@@ -14,18 +14,18 @@ const MindMap = () => {
         showLine
         defaultExpandedKeys={['0-0-0']}
       >
-        <TreeNode title="parent 1" key="0-0">
-          <TreeNode title="parent 1-0" key="0-0-0">
-            <TreeNode title="leaf" key="0-0-0-0" />
-            <TreeNode title="leaf" key="0-0-0-1" />
-            <TreeNode title="leaf" key="0-0-0-2" />
+        <TreeNode title="基础" key="0-0">
+          <TreeNode title="JavaScript" key="0-0-0">
+            <TreeNode title="语法" key="0-0-0-0" />
+            <TreeNode title="闭包" key="0-0-0-1" />
+            <TreeNode title="面向对象" key="0-0-0-2" />
           </TreeNode>
-          <TreeNode title="parent 1-1" key="0-0-1">
-            <TreeNode title="leaf" key="0-0-1-0" />
+          <TreeNode title="构建" key="0-0-1">
+            <TreeNode title="Webpack" key="0-0-1-0" />
           </TreeNode>
-          <TreeNode title="parent 1-2" key="0-0-2">
-            <TreeNode title="leaf" key="0-0-2-0" />
-            <TreeNode title="leaf" key="0-0-2-1" />
+          <TreeNode title="视图" key="0-0-2">
+            <TreeNode title="React" key="0-0-2-0" />
+            <TreeNode title="MDV" key="0-0-2-1" />
           </TreeNode>
         </TreeNode>
       </Tree>
@@ -37,7 +37,8 @@ const ChangeTab = () => {
 
   return (
     <div>
-      知识库列表
+      <p>前端</p>
+      <p>编程</p>
     </div>
   );
 };
@@ -63,7 +64,7 @@ export default class IndexPage extends React.Component {
               content={<ChangeTab />}
               trigger="click"
             >
-              <Icon type="bars" className={styles.icon + " pointer"} style={{ marginLeft: '2rem' }} />
+              <Icon type="swap" className={styles.icon + " pointer"} style={{ marginLeft: '2rem' }} />
             </Popover>
             <Badge count={0}>
               <Icon type="bell" className={styles.icon + " pointer"} style={{ marginLeft: '2rem' }} />
@@ -73,25 +74,64 @@ export default class IndexPage extends React.Component {
             <Avatar size="small" src="https://striker.teambition.net/thumbnail/110w549744d436e4ce5eb5fd2b938d90e33a/w/200/h/200" className="pointer" />
           </section>
         </Layout.Header>
-        <Layout.Content className={styles.content}>
-          <div className={styles.contentBody}>
-            <div className={styles.contentRow}>
-              <Card title="涌现" bordered={false} extra={<span href="#" className="pointer"><Icon type="edit" /></span>}>
-                暂无内容
-              </Card>
+        <Layout style={{ backgroundColor: '#fff' }}>
+          <Layout className={styles.content}>
+            <div className={styles.contentBody}>
+              <div className={styles.contentRow}>
+                <Card title={<><Icon type="bulb" /><span style={{ marginLeft: '1rem' }}>涌现</span></>} bordered={false} extra={<span className="pointer"><Icon type="edit" /></span>}>
+                  暂无内容
+                </Card>
+              </div>
+              <div className={styles.contentRow}>
+                <Card title={<><Icon type="eye" /><span style={{ marginLeft: '1rem' }}>情景</span></>} bordered={false} style={{ padding: 0 }} extra={<><span className="pointer"><Icon type="code-o" /></span><span className="pointer" style={{ marginLeft: '1rem' }}><Icon type="plus-circle" /></span></>}>
+                  <Table
+                    columns={[
+                      {
+                        title: '编号',
+                        dataIndex: 'id',
+                      },
+                      {
+                        title: '正确率',
+                        dataIndex: 'rightPee'
+                      },
+                      {
+                        title: '内容',
+                        dataIndex: 'content',
+                      },
+                      {
+                        title: '创建时间',
+                        dataIndex: 'createTime'
+                      },
+                      {
+                        title: '创建人',
+                        dataIndex: 'creator'
+                      },
+                      {
+                        title: '操作',
+                        dataIndex: 'action'
+                      }
+                    ]}
+                  />
+                </Card>
+              </div>
+              <div className={styles.contentRow}>
+                <Card title={<><Icon type="file-text" /><span style={{ marginLeft: '1rem' }}>描述</span></>} bordered={false} extra={<><span className="pointer">简化版</span><span className="pointer" style={{ marginLeft: '1rem' }}>详细版</span></>}>
+                  暂无内容
+                </Card>
+              </div>
+              <div className={styles.contentRow}>
+                <Card title={<><Icon type="filter" /><span style={{ marginLeft: '1rem' }}>原理</span></>} bordered={false}>
+                  暂无内容
+                </Card>
+              </div>
+              <div className={styles.contentRow}>
+                <Card title={<><Icon type="share-alt" /><span style={{ marginLeft: '1rem' }}>关联</span></>} bordered={false}>
+                  暂无内容
+                </Card>
+              </div>
             </div>
-            <div className={styles.contentRow}>
-              <Card title="情景" bordered={false} extra={<><span className="pointer"><Icon type="code-o" /></span><span className="pointer" style={{ marginLeft: '1rem' }}><Icon type="plus-circle" /></span></>}>
-                暂无内容
-              </Card>
-            </div>
-            <div className={styles.contentRow}>
-              <Card title="原理" bordered={false}>
-                暂无内容
-              </Card>
-            </div>
-          </div>
-        </Layout.Content>
+          </Layout>
+        </Layout>
       </Layout>
     );
   }
